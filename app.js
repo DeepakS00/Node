6091aa43e8f68dd4339159ebad9194b65a1d861c
Deepak Shukla <deepak.shukla@newput.com>
2021-09-15 22:09:19 +0530

    modification

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http'); // created a HTTP instance and stored in variable named http
 
-http.createServer(function(request, response) {
+http.createServer(function(request, response) { // a callback function to manage request and response.
     response.end('Hello Deepak Sir...!!!!!!!!!');
-}).listen(8021);
+}).listen(8021); // .listen to bind the port number to the server
