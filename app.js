6e3d5ab35131d0536eb6c8fbcca847f3ecdf5447
Deepak Shukla <deepak.shukla@newput.com>
2021-09-16 16:25:58 +0530

    done multiple routing

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-var http = require('http'); // created a HTTP instance and stored in variable named http
+const http = require('http'); // created a HTTP instance and stored in variable named http
 
-http.createServer(function(request, response) { // a callback function to manage request and response.
-    response.end('Hello Deepak Sir...!!!!!!!!!');
-}).listen(8021); // .listen to bind the port number to the server
+const hostname = "127.0.0.1";
+const port = 8021;
+
+http.createServer(function (request, response) { 
+    /* 
+        a callback function to manage request and response.
+    */
+
+    const url = request.url;
+    if (url === '/about') {
+        response.write('Hello I am Deepak Shukla'); 
+        response.end(); 
+    }
+    else if (url === '/contact') {
+        response.write('Mail me at notoriousconor404@mma.com'); 
+        response.end(); 
+    }
+    else {
+        response.write('!!!!!Welcome Welcome Welcome!!!!!'); 
+        response.end();
+    } 
+}).listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+}) 
+
+// .listen to bind the port number to the server
