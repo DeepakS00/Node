6963eb45de2320d711d3d4f275756c13acd6795f
Deepak Shukla <deepak.shukla@newput.com>
2022-12-06 17:17:47 +0530

    testing

--- NativeNode/app.js
+++ NativeNode/app.js
@@ -1,28 +1,28 @@
 const http = require('http'); // created a HTTP instance and stored in variable named http
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 8021;
 
-http.createServer(function (request, response) { 
-    /* 
-        a callback function to manage request and response.
-    */
+http
+  .createServer(function (request, response) {
+    /**
+     * A callback function to manage request and response.
+     */
 
     const url = request.url;
     if (url === '/about') {
-        response.write('Hello I am Deepak Shukla'); 
-        response.end(); 
+      response.write('Hello I am Deepak Shukla');
+      response.end();
+    } else if (url === '/contact') {
+      response.write('Mail me at notoriousconor404@mma.com');
+      response.end();
+    } else {
+      response.write('!!!!!Welcome Welcome Welcome!!!!!');
+      response.end();
     }
-    else if (url === '/contact') {
-        response.write('Mail me at notoriousconor404@mma.com'); 
-        response.end(); 
-    }
-    else {
-        response.write('!!!!!Welcome Welcome Welcome!!!!!'); 
-        response.end();
-    } 
-}).listen(port, hostname, () => {
+  })
+  .listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
-}) 
+  });
 
 // .listen to bind the port number to the server
